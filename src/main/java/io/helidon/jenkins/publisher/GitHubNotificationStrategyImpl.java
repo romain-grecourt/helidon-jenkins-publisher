d9e86171a156ea5050395ad44edf67c4fece7ffb
Romain Grecourt <romain.grecourt@oracle.com>
2019-10-17 18:50:48 -0700

    add comment

--- src/main/java/io/helidon/jenkins/publisher/GitHubNotificationStrategyImpl.java
+++ src/main/java/io/helidon/jenkins/publisher/GitHubNotificationStrategyImpl.java
@@ -45,6 +45,7 @@ public class GitHubNotificationStrategyImpl extends AbstractGitHubNotificationSt
                 }
             }
         }
+        // TODO log error
         return "#";
     }
 
